12be9dd83b5b211c3dbea4c90fb192f5c2412494
power75 <karurangod@outlook.jp>
2025-05-09 20:48:27 +0900

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <div>
+    <h1 className="text-5xl font-mono text-center">
+      ツリキタ！！！！！！！！！！！！！！！
     </h1>
+      <Image src="/image/logo.png" alt="ロゴ" width={100} height={100} />
+    </div>
   )
 }
