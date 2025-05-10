7026be11bfcca8fd9a7f32a081329e7a87a247e1
power75 <karurangod@outlook.jp>
2025-05-10 22:44:33 +0900

    shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-    <h1 className="text-5xl font-mono text-center">
-      ツリキタ！！！！！！！！！！！！！！！
-    </h1>
       <Image src="/image/logo.png" alt="ロゴ" width={1000} height={1000} />
     </div>
   )
